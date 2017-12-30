777af4eb027180b15667f9dae00002cf4ad00c2f
Petter Tomner <petter.vilhelm@gmail.com>
2017-12-30 16:44:02 +0100

    rtrety

--- blang/lang_rules/rule.py
+++ blang/lang_rules/rule.py
@@ -1,22 +1,8 @@
 
 import re
 
-class rule():
-    def text_is_this_rule(self, text, dict_of_rules):
-        i = 0
-        
-        #Split text into tokens
-        
-        token_list = self.__tokenize(text)
-        
-        for rulepoint in self.rulepoint_list:
-            for word in rulepoint.split():
-                1
-        
-    def __tokenize(self, text):
-        
-        char_is_a_token_list = [";"]
-                
+def __tokenize_rule(self, text):
+                               
         if type(text) is not str:
             RuntimeError("Parameter not a string")
         elif len(text) == 0:
@@ -51,7 +37,7 @@ class rule():
             elif c.isspace():
                 if is_in_token and not is_in_quote:
                     is_in_token = False
-                    token_list.append(text[i_start:i + 1])
+                    token_list.append(text[i_start:i])
                     i_start = i + 1
                 elif is_in_token and is_in_quote:
                     pass
@@ -112,8 +98,9 @@ class rule():
             elif not is_in_quote and (c.isalnum() or c == "_"):
                 is_in_token = True
             elif not is_in_quote: 
-                is_in_token = False
-                token_list.append(text[i_start:i])
+                if is_in_token:
+                    token_list.append(text[i_start:i])
+                    is_in_token = False
                 token_list.append(text[i:i+1])
                 i_start = i + 1
             else:
@@ -124,7 +111,130 @@ class rule():
                 break 
         
         return token_list
+    
+def __tokenize_code(self, text):
+    char_is_a_token_list = [";"]
+                
+        if type(text) is not str:
+            RuntimeError("Parameter not a string")
+        elif len(text) == 0:
+            return []
         
+        token_list = list()
+        is_in_quote = False
+        is_escaped = False
+        parentheses_count = 0
+        is_in_token = False
+        
+        i_start = 0
+        i = 0
+        while True:
+            c = text[i]
+            
+            if c == "t":
+                1
+                
+            if c == '"':
+                if is_in_quote and not is_escaped:
+                    is_in_quote = False
+                    token_list.append(text[i_start:i + 1])
+                    i_start = i + 1
+                elif is_in_quote and is_escaped:
+                    is_escaped = False
+                elif not is_in_quote:
+                    is_in_quote = True
+                    is_in_token = True
+                else:
+                    raise RuntimeError("Invalid parse:\nChar:{}\nText:\n{}".format(i, text))
+            elif c.isspace():
+                if is_in_token and not is_in_quote:
+                    is_in_token = False
+                    token_list.append(text[i_start:i])
+                    i_start = i + 1
+                elif is_in_token and is_in_quote:
+                    pass
+                elif not is_in_token:
+                    i_start = i + 1
+                else:
+                    raise RuntimeError("Invalid parse:\nChar:{}\nText:\n{}".format(i, text))
+            elif c == "(":
+                if is_in_quote:
+                    pass
+                elif not is_in_quote:
+                    if is_in_token:
+                        token_list.append(text[i_start:i])
+                        is_in_token = False
+                    elif not is_in_token:
+                        pass
+                    
+                    #find ending parantheses
+                    parentheses_count = 1
+                    i_start_par = i
+                    in_quote_par = False
+                    is_escaped_par = False
+                     
+                    for ii in range(i + 1,len(text)):
+                        c = text[ii]
+                        if c == ")" and not in_quote_par:
+                            parentheses_count -= 1
+                            if parentheses_count == 0:
+                                token = self.__tokenize(text[i_start_par + 1:ii]) # Recursive tokenize contents of parantheses
+                                token_list.append(token)
+                                i_start = ii + 1
+                                i = ii
+                                break
+                        elif c == ")" and in_quote_par:
+                            pass
+                        elif c == "(" and not in_quote_par:
+                            parentheses_count += 1
+                        elif c == "(" and in_quote_par:
+                            pass                            
+                        elif c == '"' and not in_quote_par:
+                            in_quote_par = True
+                        elif c == '"' and in_quote_par and not is_escaped_par:
+                            in_quote_par = False   
+                        elif c == '"' and in_quote_par and is_escaped_par:
+                            pass
+                        elif c == "\\" and in_quote_par and not is_escaped_par:
+                            is_escaped = True
+                        elif c == "\\" and in_quote_par and is_escaped_par:
+                            pass
+                        elif c == "\\" and not in_quote_par:
+                            pass
+                         
+                        
+                        if ii + 1 == len(text):
+                            raise RuntimeError("Invalid parse (no end parantheses) :\nChar:{}\nText:\n{}".format(i_start_par, text))
+                        
+                    i_start = ii + 1
+            elif not is_in_quote and (c.isalnum() or c == "_"):
+                is_in_token = True
+            elif not is_in_quote: 
+                if is_in_token:
+                    token_list.append(text[i_start:i])
+                    is_in_token = False
+                token_list.append(text[i:i+1])
+                i_start = i + 1
+            else:
+                pass
+            
+            i += 1
+            if i == len(text):
+                break 
+        
+        return token_list
+
+class rule():
+    def text_is_this_rule(self, text, dict_of_rules):
+        i = 0
+        
+        #Split text into tokens
+        
+        
+        
+        for rulepoint in self.rulepoint_list:
+            token_list = __tokenize_rule(rulepoint)
+            
     
     def __init__(self, text):
         self.text = text
--- blang/lang_rules/rule_parser.py
+++ blang/lang_rules/rule_parser.py
@@ -1,4 +1,5 @@
 import re
+from blang.lang_rules.rule import rule 
 
 class rule_parser(object):
     """description of class"""
@@ -35,9 +36,45 @@ class rule_parser(object):
                 i += 1
 
             self.text_no_comments += line[0:i] + '\n'
+        
+        #now parse the rules    
+        dict_of_rules = {}
+        list_of_raw_rules = []
+        in_rule = False
+        s = ""
+        
+        for line in self.text_no_comments.splitlines(keepends = True):
+            m = re.search(r"^([\w\-]*) \([^\)]*\):", line)
+            if (m is not None) and not in_rule:
+                in_rule = True
+                s = line
+            elif (m is not None) and in_rule:
+                if len(s) > 0 :
+                    list_of_raw_rules.append(s)
+                s = line
+            elif in_rule:
+                m = re.match(r"    (.*)", line)
+                if m is None:
+                    list_of_raw_rules.append(s)
+                    s = ""
+                    in_rule = False
+                else:
+                    s += line
+                    
+        if len(s) > 0:
+            list_of_raw_rules.append(s)
+                 
+        for raw_rule in list_of_raw_rules:
+            r = rule(raw_rule)
+            dict_of_rules[r.short_name] = r
+            
+        return dict_of_rules
+            #r = rule(self.text_no_comments)
+        
 
 if __name__ == "__main__":
     p = rule_parser()
     p.load(r"C:\repos\blang\blang\blang\bläng spec.txt")
     p.parse()
-    print(p.text_no_comments)
+    
+    
